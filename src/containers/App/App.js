ae9c954d3bcedf045d0f1d94781d304869b81e3b
Matthew Malone <m.malone@me.com>
2019-11-06 09:31:13 -0700

    Make App into a functional component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,33 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import { setOrders } from '../../actions';
-// import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
-export class App extends Component {
-  constructor(props) {
-    super();
-  }
+export const App = () => {
+  return (
+    <main className="App">
+      <header>
+        <h1>Burrito Builder</h1>
+        <OrderForm />
+      </header>
+      
+      <Orders />
 
-
-
-  render() {
-    return (
-      <main className="App">
-        <header>
-          <h1>Burrito Builder</h1>
-          <OrderForm />
-        </header>
-        
-        <Orders />
-
-      </main>
-    );
-  }
+    </main>
+  );
 }
-
 
 export default App
