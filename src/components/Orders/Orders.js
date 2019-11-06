17d734e0224113e78f99f3b6cabeeed8bdf2d1eb
Matthew Malone <m.malone@me.com>
2019-11-06 09:29:05 -0700

    Remove Redux and fetch from App to Orders.js

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Orders.css';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { setOrders } from '../../actions';
+import { getOrders } from '../../apiCalls';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
-    return (
-      <div className="order">
-        <h3>{order.name}</h3>
-        <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-      </div>
-    )
-  });
+export class Orders extends Component {
+  constructor(props) {
+    super();
+    // this.props = props;
+  }
 
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+  componentDidMount() {
+    getOrders()
+      .then(data => this.props.setOrders(data.orders))
+      .catch(err => console.error('Error fetching:', err));
+  }
+
+
+  orders = () => {
+    // const orderEls = this.props.orders
+    this.props.orders.map(order => {
+      return (
+        <div className="order">
+          <h3>{order.name}</h3>
+          <ul className="ingredient-list">
+            {order.ingredients.map(ingredient => {
+              return <li>{ingredient}</li>
+            })}
+          </ul>
+        </div>
+      )
+    });
+  }
+
+    render() {
+      return (
+        <section>
+          { this.props.orders.length ? this.orders : <p>No orders yet!</p> }
+        </section>
+      )
+    }
 }
 
-export default Orders;
+
+export const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    setOrders,
+  }, dispatch)
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
+
+// export default Orders;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { setOrders } from '../../actions';
-import { getOrders } from '../../apiCalls';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import { setOrders } from '../../actions';
+// import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 export class App extends Component {
   constructor(props) {
     super();
-    this.props = props;
   }
 
-  componentDidMount() {
-    getOrders()
-      .then(data => this.props.setOrders(data.orders))
-      .catch(err => console.error('Error fetching:', err));
-  }
+
 
   render() {
     return (
@@ -27,21 +22,12 @@ export class App extends Component {
           <OrderForm />
         </header>
         
-        <Orders orders={this.props.orders}/>
+        <Orders />
+
       </main>
     );
   }
 }
 
-export const mapStateToProps = ({ orders }) => ({
-  orders
-});
 
-export const mapDispatchToProps = dispatch => (
-  bindActionCreators({
-    setOrders,
-  }, dispatch)
-);
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
+export default App
